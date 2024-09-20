240bfcab38b2f0d7e77099b3d65bf23b28873298
seancleve <seanycleve@gmail.com>
2024-09-20 09:49:05 -0400

    Lab 04 02

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,19 @@ public class Main
 {
     public static void main(String[] args)
     {
+        double winterCost = 40;
+        double springCost = 50;
+        double summerCost = 60;
+        double fallCost = 70;
+
+        double yearlyCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("Winter Maintenance Cost: $" + winterCost);
+        System.out.println("Spring Maintenance Cost: $" + springCost);
+        System.out.println("Summer Maintenance Cost: $" + summerCost);
+        System.out.println("Fall Maintenance Cost: $" + fallCost);
+        System.out.println("Total Yearly Maintenance Cost: $" + yearlyCost);
 
 
-
-
-
-        
     }
 }
